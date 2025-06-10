5aa5465081287f0e363caaf5fb2bdfb69ac80d80
Salman butt <salman0butt@gmail.com>
2025-06-10 17:58:50 +0500

    feat: Add migration to make location column nullable in stores table

---
+++ database/migrations/2025_06_10_125211_make_location_nullable_in_stores_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Check if the spatial index exists and drop it
+        $indexes = DB::select("SHOW INDEX FROM stores WHERE Key_name = 'idx_stores_location'");
+        if (!empty($indexes)) {
+            DB::statement('DROP INDEX idx_stores_location ON stores');
+        }
+        
+        // Alter the location column to be nullable
+        DB::statement('ALTER TABLE stores MODIFY COLUMN location POINT NULL SRID 4326');
+        
+        // Note: Spatial indexes cannot be created on nullable columns in MySQL
+        // We'll skip recreating the index for now since location is not being used
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Make the location column NOT NULL again first
+        DB::statement('ALTER TABLE stores MODIFY COLUMN location POINT NOT NULL SRID 4326');
+        
+        // Recreate the spatial index
+        DB::statement('CREATE SPATIAL INDEX idx_stores_location ON stores(location)');
+    }
+};
